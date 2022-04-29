18fed55cab8fa66e5bf85d248c18f1349bbfc1b6
Vidit Kumar Singh <vidit.singh@incedoinc.com>
2022-04-29 13:03:13 +0530

    Access modifier updaete

--- OOps/src/com/accessScope/examples/AccessModifier.java
+++ OOps/src/com/accessScope/examples/AccessModifier.java
@@ -2,15 +2,16 @@ package com.accessScope.examples;
 
 
 // Tips to remember :
-//Private intance variables neither be called directly in main() nor inherited.
+//Private instance variables neither be called directly in main() nor inherited.
 // if there is not any access modifier then by default "default" modifier is applied on every method class or instance variables,
-// instance veriable needs to be static if it has been used in static method.
+// instance variable needs to be static if it has been used in static method.
 //Classes or interface can not be declared as private
 
-// The methods or data members declared as protected are accessible within same package or sub classes in different package.
+// The methods or data members declared as protected are accessible within same package or subclasses in different package.
 
 public class AccessModifier {
 
+	private int priv = 10;
 	static int x = 10 ;
 	int var = 20;
 	
@@ -18,6 +19,7 @@ public class AccessModifier {
 
 		//System.out.println("var :" + var);  // compile time error, since var is not declared as static.
 		System.out.println("X :" + x);
+//		System.out.println(priv); // compile time error as instance variable is private.
 	}
 
 }
